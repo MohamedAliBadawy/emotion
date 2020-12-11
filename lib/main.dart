e37d45448436d39821a946a7f921f0d2e1264e3e
medoali10 <medooali6262@gmail.com>
2020-12-12 00:14:23 +0200

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// import 'dart:html';
-
 import 'dart:io';
 import 'dart:math' as math;
 
@@ -13,8 +11,7 @@ import './Box.dart';
 
 List<CameraDescription> cameras;
 
-Future<Null> main() async
-{
+Future<Null> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
     cameras = await availableCameras();
@@ -37,7 +34,6 @@ class FlutterTeachable extends StatefulWidget {
 }
 
 class _FlutterTeachableState extends State<FlutterTeachable> {
-
   bool liveFeed = false;
 
   List<dynamic> _recognitions;
@@ -57,36 +53,31 @@ class _FlutterTeachableState extends State<FlutterTeachable> {
     super.dispose();
     Tflite.close();
   }
+
   @override
   void initState() {
     super.initState();
 
     _load = true;
 
-    loadMyModel().then((v){
+    loadMyModel().then((v) {
       setState(() {
         _load = false;
       });
     });
-
   }
 
-  loadMyModel() async
-  {
+  loadMyModel() async {
     var res = await Tflite.loadModel(
-      labels: "assets/labels.txt",
-      model: "assets/model_unquant.tflite"
-    );
+        labels: "assets/labels.txt", model: "assets/model_unquant.tflite");
 
     print("Result after Loading the Model is : $res");
-
   }
 
-  chooseImage() async
-  {
+  chooseImage() async {
     File _img = await ImagePicker.pickImage(source: ImageSource.camera);
 
-    if(_img == null) return;
+    if (_img == null) return;
 
     setState(() {
       _load = true;
@@ -95,27 +86,28 @@ class _FlutterTeachableState extends State<FlutterTeachable> {
     });
   }
 
-  applyModelonImage(File file) async
-  {
+  applyModelonImage(File file) async {
     var _res = await Tflite.runModelOnImage(
-      path: file.path,
-      numResults: 2,
-      threshold: 0.5,
-      imageMean: 127.5,
-      imageStd: 127.5
-    );
+        path: file.path,
+        numResults: 2,
+        threshold: 0.5,
+        imageMean: 127.5,
+        imageStd: 127.5);
 
     setState(() {
       _load = false;
       _result = _res;
       print(_result);
       String str = _result[0]["label"];
-      
+
       _fingers = str.substring(2);
-      _confidence = _result != null ? (_result[0]["confidence"]*100.0).toString().substring(0,2) + "%" : "";
+      _confidence = _result != null
+          ? (_result[0]["confidence"] * 100.0).toString().substring(0, 2) + "%"
+          : "";
 
       print(str.substring(2));
-      print((_result[0]["confidence"]*100.0).toString().substring(0,2)+"%");
+      print(
+          (_result[0]["confidence"] * 100.0).toString().substring(0, 2) + "%");
       print("indexed : ${_result[0]["label"]}");
     });
   }
@@ -126,7 +118,7 @@ class _FlutterTeachableState extends State<FlutterTeachable> {
       _imageHeight = imageHeight;
       _imageWidth = imageWidth;
     });
-  }  
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -135,40 +127,52 @@ class _FlutterTeachableState extends State<FlutterTeachable> {
       appBar: AppBar(
         title: Text("Teachable Machine with FLUTTER"),
       ),
-      body: !liveFeed ? Center(
-        child: ListView(
-          children: <Widget>[
-            _load ? Container(alignment: Alignment.center,child: CircularProgressIndicator(),)
-              : Container(
-                width: size.width*0.9,
-                height: size.height*0.7,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Center(
-                      child: _pic != null ? Image.file(_pic,width: size.width*0.6,) : Container(),
-                    ),
-                    _result == null ? Container()
-                          : Text("Number of Fingers : $_fingers\nConfidence: $_confidence"),
-                  ],
-                ),
-              )
-          ],
-        ),
-      )
-      : Stack(
+      body: !liveFeed
+          ? Center(
+              child: ListView(
+                children: <Widget>[
+                  _load
+                      ? Container(
+                          alignment: Alignment.center,
+                          child: CircularProgressIndicator(),
+                        )
+                      : Container(
+                          width: size.width * 0.9,
+                          height: size.height * 0.7,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Center(
+                                child: _pic != null
+                                    ? Image.file(
+                                        _pic,
+                                        width: size.width * 0.6,
+                                      )
+                                    : Container(),
+                              ),
+                              _result == null
+                                  ? Container()
+                                  : Text(
+                                      "Number of Fingers : $_fingers\nConfidence: $_confidence"),
+                            ],
+                          ),
+                        )
+                ],
+              ),
+            )
+          : Stack(
               children: [
                 CameraLiveScreen(
                   widget.cameras,
                   setRecognitions,
                 ),
                 EnclosedBox(
-                    _recognitions == null ? [] : _recognitions,
-                    math.max(_imageHeight, _imageWidth),
-                    math.min(_imageHeight, _imageWidth),
-                    size.height,
-                    size.width,
+                  _recognitions == null ? [] : _recognitions,
+                  math.max(_imageHeight, _imageWidth),
+                  math.min(_imageHeight, _imageWidth),
+                  size.height,
+                  size.width,
                 ),
               ],
             ),
@@ -179,7 +183,7 @@ class _FlutterTeachableState extends State<FlutterTeachable> {
           children: <Widget>[
             RaisedButton(
               child: Text("Image Feed"),
-              onPressed: (){
+              onPressed: () {
                 setState(() {
                   liveFeed = false;
                 });
